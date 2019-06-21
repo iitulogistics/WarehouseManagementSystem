83c1c1feece9672f36a88e4a2fb69342cf995f53
Karim <karim20.ks@gmail.com>
2019-06-21 16:11:34 +0600

    Добавлен поиск по LifeCycle и обновление LifeCycle и IdStillage

--- src/main/java/com/example/wms/wms/repositories/ContainerRepository.java
+++ src/main/java/com/example/wms/wms/repositories/ContainerRepository.java
@@ -12,10 +12,10 @@ import java.util.List;
 public interface ContainerRepository extends JpaRepository<ContainerEntity, Long> {
 
     @Query("select p from ContainerEntity p where p.stillageId = ?1")
-    List<ContainerEntity> getPalletsByStillageId(Long id);
+    List<ContainerEntity> getContainersByStillageId(Long id);
 
     @Query("select p from ContainerEntity p where p.product_id = ?1")
-    List<ContainerEntity> getPalletsByProductId(Long id);
+    List<ContainerEntity> getContainersByProductId(Long id);
 
     @Modifying(clearAutomatically = true)
     @Transactional
@@ -26,4 +26,12 @@ public interface ContainerRepository extends JpaRepository<ContainerEntity, Long
     @Transactional
     @Query("update ContainerEntity p set p.lifeCycle = ?2 where p.id = ?1")
     void updateLifeCyrcleById(Long id, BaseType.LifeCycle lifeCycle);
+
+    @Modifying(clearAutomatically = true)
+    @Transactional
+    @Query("update ContainerEntity p set p.lifeCycle = ?2, p.stillageId = ?3 where p.id = ?1")
+    void updateLifeCyrcleAndStillageById(Long id, BaseType.LifeCycle lifeCycle, Long id_stillage);
+
+    @Query("select c from ContainerEntity c where c.lifeCycle = ?1")
+    List<ContainerEntity> getContainerByLifeCycle(BaseType.LifeCycle lifeCycle);
 }
