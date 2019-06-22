95a16083338366e493f6cca3e77e2bdbe67c6a8e
Karim <karim20.ks@gmail.com>
2019-06-22 17:35:34 +0600

    Контролер для задач

---
+++ src/main/java/com/example/wms/wms/controllers/TaskController.java
@@ -0,0 +1,44 @@
+package com.example.wms.wms.controllers;
+
+import com.example.wms.wms.entities.TaskEntity;
+import com.example.wms.wms.repositories.TaskRepository;
+import io.swagger.annotations.Api;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
+
+@Api(tags = {"Задачи"}, description = "API контроля задач")
+@RestController
+@RequestMapping("/task")
+public class TaskController {
+    private final TaskRepository taskRepository;
+
+    TaskController(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @PostMapping("/all")
+    public List<TaskEntity> getAll() {
+        return taskRepository.findAll();
+    }
+
+    @PostMapping("/allCurrentTasks")
+    public List<TaskEntity> getAllCurrentTasks() {
+        return taskRepository.getCurrentTasks();
+    }
+    @PostMapping("/allCurrentTasks")
+    public List<TaskEntity> getAllDoneTasks() {
+        return taskRepository.getDoneTasks();
+    }
+
+    @PostMapping("/done")
+    public ResponseEntity<?> taskDone(Long id) {
+        taskRepository.taskDone(id, new Date());
+        return ResponseEntity.ok("Задача с id = " + id + " выполнена");
+    }
+
+}
--- src/main/java/com/example/wms/wms/repositories/TaskRepository.java
+++ src/main/java/com/example/wms/wms/repositories/TaskRepository.java
@@ -2,6 +2,23 @@ package com.example.wms.wms.repositories;
 
 import com.example.wms.wms.entities.TaskEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.List;
+
 
 public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
+    @Modifying(clearAutomatically = true)
+    @Transactional
+    @Query("update TaskEntity task set task.done = ?2 where task.id = ?1")
+    void taskDone(Long id, Date date);
+
+    @Query("select task from TaskEntity task where task.done is null")
+    List<TaskEntity> getCurrentTasks();
+
+    @Query("select task from TaskEntity task where task.done is not null")
+    List<TaskEntity> getDoneTasks();
 }
