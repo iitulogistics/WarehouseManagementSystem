aa5022f275d9d890c1f3026ee0801587a0583b16
Karim <karim20.ks@gmail.com>
2019-06-27 13:50:25 +0600

    Поиск свободных ячеек стиллажа

--- src/main/java/com/example/wms/wms/repositories/StillageRepository.java
+++ src/main/java/com/example/wms/wms/repositories/StillageRepository.java
@@ -6,10 +6,15 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 public interface StillageRepository extends JpaRepository<StillageEntity, Long> {
 
     @Modifying(clearAutomatically = true)
     @Transactional
     @Query("update StillageEntity s set s.max_count_object = ?2 where s.id = ?1")
     void updateMaxCountObject(Long id,  int count);
+
+    @Query("select s from StillageEntity s where not s.max_count_object = s.count_object")
+    List<StillageEntity> getLooseStillage();
 }
