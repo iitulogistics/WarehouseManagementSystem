b047832237b669b4a025fe6be3dd09b6f35bfb38
Karim <karim20.ks@gmail.com>
2019-07-11 10:46:35 +0600

    Добавлен фронт для статистики и стеллажей

--- src/main/java/com/example/wms/wms/controllers/StatisticsController.java
+++ src/main/java/com/example/wms/wms/controllers/StatisticsController.java
@@ -7,11 +7,15 @@ import com.example.wms.wms.repositories.StillageRepository;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 @RestController
 @Api(tags = "Статистика", description = "Api для выдачи статистики на складе")
@@ -24,6 +28,30 @@ public class StatisticsController {
                                 ProductRepository productRepository) {
         this.stillageRepository = stillageRepository;
         this.productRepository = productRepository;
+    }
+
+    @GetMapping("")
+    public ModelAndView main() {
+        Map<String, Object> root = new TreeMap<>();
+
+        List<StillageEntity> stillageEntities = stillageRepository.findAll();
+        int looseStillage = 0,
+                withContainer = 0;
+
+        for (StillageEntity stillage : stillageEntities) {
+            if (stillage.getCount_object() == 0) {
+                looseStillage++;
+            } else if (stillage.getCount_object() != stillage.getMax_count_object()) {
+                withContainer++;
+            }
+        }
+
+        root.put("count", stillageEntities.size());
+        root.put("looseStillage", looseStillage);
+        root.put("withContainer", withContainer);
+        root.put("busy", (stillageEntities.size() - looseStillage - withContainer));
+
+        return new ModelAndView("statistics", root);
     }
 
     @ApiOperation("Вывод информации по стеллажам")
@@ -41,12 +69,33 @@ public class StatisticsController {
             }
         }
 
-
         return ResponseEntity.ok("На складе " + stillageEntities.size() + " стеллажных ячеек\n" +
                 looseStillage + " свободных ячеек\n" +
                 withContainer + " полусвободных ячеек\n" +
                 (stillageEntities.size() - looseStillage - withContainer) + " стеллажей занято");
     }
+//
+//    @ApiOperation("Вывод информации по стеллажам")
+//    @PostMapping("/countStillage")
+//    public ResponseEntity<?> getCountStillage(){
+//        return ResponseEntity.ok(stillageRepository.findAll().size());
+//    }
+//
+//    @ApiOperation("Вывод информации по стеллажам")
+//    @PostMapping("/countLooseStillage")
+//    public ResponseEntity<?> getCountLooseStillage(){
+//        int looseStillage = 0,
+//                withContainer = 0;
+//
+//        for(StillageEntity stillage : stillageRepository.getLooseStillage()){
+//            if (stillage.getCount_object() == 0) {
+//                looseStillage++;
+//            }
+//        }
+//
+//        return ResponseEntity.ok(looseStillage);
+//    }
+
 
     @ApiOperation("Вывод информации по товарам")
     @PostMapping("/productInfo")
--- src/main/java/com/example/wms/wms/controllers/StillageController.java
+++ src/main/java/com/example/wms/wms/controllers/StillageController.java
@@ -1,7 +1,11 @@
 package com.example.wms.wms.controllers;
 
 import com.example.wms.wms.base.BaseType;
+import com.example.wms.wms.entities.ContainerEntity;
+import com.example.wms.wms.entities.ProductEntity;
 import com.example.wms.wms.entities.StillageEntity;
+import com.example.wms.wms.repositories.ContainerRepository;
+import com.example.wms.wms.repositories.ProductRepository;
 import com.example.wms.wms.repositories.StillageRepository;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -10,9 +14,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Collection;
-import java.util.List;
+import java.lang.reflect.Array;
+import java.util.*;
 
 //										Стандартные размеры стилажей
 //					Высота									Длина							Глубина
@@ -24,6 +29,8 @@ import java.util.List;
 public class StillageController {
 
     private final StillageRepository repository;
+    private final ProductRepository productRepository;
+    private final ContainerRepository containerRepository;
 
     @Value("${standard.pallet.width}")
     double pallet_width;
@@ -31,8 +38,50 @@ public class StillageController {
     double pallet_length;
 
     @Autowired
-    public StillageController(StillageRepository repository) {
+    public StillageController(StillageRepository repository,
+                              ProductRepository productRepository,
+                              ContainerRepository containerRepository) {
         this.repository = repository;
+        this.productRepository = productRepository;
+        this.containerRepository = containerRepository;
+    }
+
+    @GetMapping("")
+    public ModelAndView main() {
+        List<StillageEntity> stillageEntities = repository.findAll();
+        stillageEntities.sort(new Comparator<StillageEntity>() {
+            @Override
+            public int compare(StillageEntity o1, StillageEntity o2) {
+                return (o1.getStillage_index() == o2.getStillage_index() ?
+                        (o1.getShelf_index() - o2.getShelf_index()) : (o1.getStillage_index() - o2.getStillage_index()));
+            }
+        });
+
+        Map<String, Object> root = new TreeMap<>();
+
+        int last_index = 0;
+        int stillage_index = 0;
+        List<Object[]> list = new ArrayList<>();
+        List<Object[]> final_list = new ArrayList<>();
+
+        for (int i = 0; i < stillageEntities.size(); i++) {
+            if (last_index != stillageEntities.get(i).getStillage_index() || i == stillageEntities.size() - 1) {
+                final_list.add(new Object[]{stillage_index, list});
+                list = new ArrayList<>();
+                last_index = stillage_index;
+            } else {
+                StringBuilder info = new StringBuilder();
+                for(ContainerEntity container : containerRepository.getContainersByStillageId(stillageEntities.get(i).getId())){
+                    info.append("Контейнер №").append(container.getId()).append(": ")
+                            .append(productRepository.getOne(container.getProduct_id()).getProduct_name())
+                            .append(" ").append(container.getCount_product()).append("шт.<br>");
+                }
+                stillage_index = stillageEntities.get(i).getStillage_index();
+                list.add(new Object[]{stillageEntities.get(i).getShelf_index(), (info.toString().isEmpty() ? "Пуст" : info.toString()), stillageEntities.get(0).getMax_count_object()});
+            }
+        }
+        root.put("stillages", final_list);
+        return new ModelAndView("stillage", root);
     }
 
     @ApiOperation("Добавить стеллаж")
