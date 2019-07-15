745559f6c3eea051a44406bb02e09421f7d1001d
Karim <karim20.ks@gmail.com>
2019-07-15 21:15:03 +0600

    Добавление продукта по параметрам

--- src/main/java/com/example/wms/wms/controllers/ProductController.java
+++ src/main/java/com/example/wms/wms/controllers/ProductController.java
@@ -1,14 +1,10 @@
 package com.example.wms.wms.controllers;
 
-import com.example.wms.wms.base.BaseType;
 import com.example.wms.wms.entities.ProductEntity;
 import com.example.wms.wms.repositories.ProductRepository;
-import com.fasterxml.jackson.databind.type.TypeBase;
-import com.sun.org.apache.regexp.internal.RE;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -21,80 +17,58 @@ import java.util.TreeMap;
 //@RequestMapping(value = "/product")
 public class ProductController {
 
-    private final ProductRepository repository;
+	private final ProductRepository repository;
 
-    @GetMapping("/")
-    public ModelAndView index() {
-        Map<String, Object> root = new TreeMap<>();
+	@GetMapping("/")
+	public ModelAndView index() {
+		Map<String, Object> root = new TreeMap<>();
 
-        root.put("products", showAll());
-        root.put("last_search", "");
-        return new ModelAndView("index", root);
-    }
+		root.put("products", showAll());
+		root.put("last_search", "");
+		return new ModelAndView("index", root) ;
+	}
 
-    @PostMapping("/")
-    public ModelAndView getProdLikeName(@RequestParam(name = "name") String name) {
-        Map<String, Object> root = new TreeMap<>();
+	@PostMapping("/")
+	public ModelAndView getProdLikeName(@RequestParam(name = "name") String name) {
+		Map<String, Object> root = new TreeMap<>();
 
-        root.put("products", getProductLikeName(name));
-        root.put("last_search", name);
-        return new ModelAndView("index", root);
-    }
+		root.put("products", getProductLikeName(name));
+		root.put("last_search", name);
+		return new ModelAndView("index", root);
+	}
 
-    @Autowired
-    public ProductController(ProductRepository repository) {
-        this.repository = repository;
-    }
+	@Autowired
+	public ProductController(ProductRepository repository) {
+		this.repository = repository;
+	}
 
-    @ApiOperation("Добавить продукт")
-    @PostMapping("/add")
-    public ResponseEntity<?> addProduct(@RequestBody ProductEntity productEntity) {
-        repository.save(productEntity);
-        return ResponseEntity.ok("Товар добавлен");
-    }
+	@ApiOperation("Добавить продукт")
+	@PostMapping("/add")
+	public void addProduct(@RequestBody ProductEntity productEntity){
+		repository.save(productEntity);
+	}
 
-    @ApiOperation("Добавить продукт")
-    @PostMapping("/addProductByParam")
-    public ResponseEntity<?> addProductByParam(@RequestParam(name = "product_name") String productName,
-                                  @RequestParam(name = "length") double length,
-                                  @RequestParam(name = "width") double width,
-                                  @RequestParam(name = "height") double height,
-                                  @RequestParam(name = "weight") double weight,
-                                  @RequestParam(name = "price") double price,
-                                  @RequestParam(name = "type_product") BaseType.TypeProduct product) {
-        ProductEntity productEntity = new ProductEntity();
-        productEntity.setProduct_name(productName);
-        productEntity.setLength(length);
-        productEntity.setWidth(width);
-        productEntity.setHeight(height);
-        productEntity.setWeight(weight);
-        productEntity.setPrice(price);
-        productEntity.setType_product(product);
-        return addProduct(productEntity);
-    }
+	@ApiOperation("Показать все продукты")
+	@PostMapping("/all")
+	public List<ProductEntity> showAll(){
+		return repository.findAll();
+	}
 
+	@ApiOperation("Показать продукт по имени")
+	@PostMapping("/getProductByName")
+	public List<ProductEntity> getProductByName(@RequestParam String name){
+		return repository.getProductByName(name);
+	}
 
-    @ApiOperation("Показать все продукты")
-    @PostMapping("/all")
-    public List<ProductEntity> showAll() {
-        return repository.findAll();
-    }
+	@ApiOperation("Показать продукт по имени")
+	@PostMapping("/getProductLikeName")
+	public List<ProductEntity> getProductLikeName(@RequestParam String name){
+		return repository.getProductLikeName(name);
+	}
 
-    @ApiOperation("Показать продукт по имени")
-    @PostMapping("/getProductByName")
-    public List<ProductEntity> getProductByName(@RequestParam String name) {
-        return repository.getProductByName(name);
-    }
-
-    @ApiOperation("Показать продукт по имени")
-    @PostMapping("/getProductLikeName")
-    public List<ProductEntity> getProductLikeName(@RequestParam String name) {
-        return repository.getProductLikeName(name);
-    }
-
-    @ApiOperation("Удалить продукт")
-    @DeleteMapping("/deleteById")
-    public void deleteProduct(@RequestParam(name = "id") Long id) {
-        repository.delete(repository.getOne(id));
-    }
+	@ApiOperation("Удалить продукт")
+	@DeleteMapping("/deleteById")
+	public void deleteProduct(@RequestParam(name = "id") Long id){
+		repository.delete(repository.getOne(id));
+	}
 }
