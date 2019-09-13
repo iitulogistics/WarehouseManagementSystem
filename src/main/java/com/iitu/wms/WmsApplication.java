f7f6b7eaaed42f28c09158dd0d02a0ac6b8338b8
DMyrzakanov <U$er1234>
2019-09-13 17:06:14 +0600

    initial commit with eureka client

--- src/main/java/com/iitu/wms/WmsApplication.java
+++ src/main/java/com/iitu/wms/WmsApplication.java
@@ -2,8 +2,10 @@ package com.iitu.wms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class WmsApplication {
 
 	public static void main(String[] args) {
